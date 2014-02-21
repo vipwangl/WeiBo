072bcfd725a8244fff3ffdbf03568b2228eddb2d
Mr Wang <mrwang@MrdeiMac.local>
2014-02-21 23:29:44 +0800

    Initial Commit

---
+++ WeiBoDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WeiBoDemo
+//
+//  Created by Mr Wang on 14-2-21.
+//  Copyright (c) 2014年 Mama. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
